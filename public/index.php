56f5f880c2226969dc486c1d0d2b8a88437ff962
GabriellaP29 <146529638+GabriellaP29@users.noreply.github.com>
2023-12-09 09:42:31 +0000

    Update index.php

--- public/index.php
+++ public/index.php
@@ -15,9 +15,18 @@
 </div>
 <main>
     <h3> Welcome to SportsFun online store</h3>
-    <div class="welcome">
-           hi
-    </div>
+    <h2> About this online store</h2>
+
+<div class="info">
+    <p> Here at SportsFun we have a huge selection of items in sale of up to 80% discount! <br>  If you want to know more keep reading.
+    </p>
+</div>
+
+<div class="discount"> <h4>For the month of December for every new costumer registered in our website we will give a 10% discount!  </h4>
+    <p> <a href="../pdo/register.php">Register now!</a> </p></div>
+
+<?php require_once '../template/footer.php';?>
+
 </main>
 
 <?php require_once '../template/footer.php';
